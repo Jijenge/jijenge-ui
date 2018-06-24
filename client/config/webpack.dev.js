12b89f21ae4c17be2387fde4d895997dc02c9f48
IsabellaBeltran <isabellabeltran79@gmail.com>
2018-06-23 22:43:09 -0400

    Tested image compiler

--- client/config/webpack.dev.js
+++ client/config/webpack.dev.js
@@ -73,17 +73,6 @@ module.exports = {
           }
         ]
       },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: "url-loader", 
-            options: {
-              limit: 8192
-            }
-          }
-        ]
-      }
     ]
   },
   plugins: [
