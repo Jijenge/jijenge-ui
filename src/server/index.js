df294a816dca87206fa920f395b32b49ea9411a7
IsabellaBeltran <isabellabeltran79@gmail.com>
2018-08-01 21:55:56 -0400

    Change file structure for easy access

--- src/server/index.js
+++ src/server/index.js
@@ -2,9 +2,9 @@ import express from 'express';
 import path from 'path';
 const server = express(); 
 
-// const isProd = process.env.NODE_ENV === 
-// "production"
-// if (!isProd) {
+const isProd = process.env.NODE_ENV === 
+"production"
+if (!isProd) {
 const webpack = require('webpack'); 
 const config = require('../../config/webpack.dev');
 const compiler = webpack(config); 
@@ -15,7 +15,7 @@ const webpackDevMiddleware = require('webpack-dev-middleware')(
 const webpackHotMiddleware = require('webpack-hot-middleware')(compiler);
 server.use(webpackDevMiddleware); 
 server.use(webpackHotMiddleware);
-// }
+}
 
 const staticMiddleware = express.static('dist'); 
 
@@ -23,7 +23,7 @@ server.use(staticMiddleware);
 
 server.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')));
 
-// const PORT = process.env.PORT || 6060; 
+const PORT = process.env.PORT || 6060; 
 
 server.listen(6060, () => {
   console.log('Successfully connected to port 6060'); 
