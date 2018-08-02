e5f0d417f3860f0f13222fa5bf92f7251d010076
IsabellaBeltran <isabellabeltran79@gmail.com>
2018-08-01 20:02:35 -0400

    Preparing for prodution

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
-require("babel-runtime/regenerator");
+// require("babel-runtime/regenerator");
+// require("webpack-hot-middleware/client?reload=true");
 require("react-hot-loader/patch");
-require("webpack-hot-middleware/client?reload=true");
 require("babel-register");
 require("./index.html");
 require("./main.scss");
--- client/src/server/index.js
+++ client/src/server/index.js
@@ -1,21 +1,21 @@
 import express from 'express'; 
 import path from 'path';
-
 const server = express(); 
 
+const isProd = process.env.NODE_ENV === 
+"production"
+if (!isProd) {
 const webpack = require('webpack'); 
 const config = require('../../config/webpack.dev');
 const compiler = webpack(config); 
-
 const webpackDevMiddleware = require('webpack-dev-middleware')(
   compiler,
   config.devServer
 );
-
 const webpackHotMiddleware = require('webpack-hot-middleware')(compiler);
-
 server.use(webpackDevMiddleware); 
 server.use(webpackHotMiddleware);
+}
 
 const staticMiddleware = express.static('dist'); 
 
@@ -23,6 +23,8 @@ server.use(staticMiddleware);
 
 server.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')));
 
-server.listen(6060, () => {
+const PORT = process.env.PORT || 6060; 
+
+server.listen(PORT, () => {
   console.log('Successfully connected to port 6060'); 
 }); 
