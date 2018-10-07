c2be3e1e8b1d9b7b5dba79457973e70709018ae2
cmichaelsd <cmichaelsd@gmail.com>
2018-10-07 00:44:30 -0700

    Altered give monthly controller

--- src/rest-server/controllers/monthlyDonation.js
+++ src/rest-server/controllers/monthlyDonation.js
@@ -6,16 +6,43 @@ const monthlyDonation = {
     const input = JSON.parse(req.body);
     console.log(typeof input, input);
     try {
-      let { status } = await stripe.charges.create({
-        receipt_email: input.email,
-        amount: input.amount,
+      // Define a Product
+      const product = await stripe.products.create({
+        name: "Give Monthly",
+        type: "service"
+      });
+      console.log("PRODUCT", product);
+      // Define Products Plan
+      const varPlan = await stripe.plans.create({
+        product: product.id,
+        nickname: "Give Monthly USD",
         currency: "usd",
-        description: "Jijenge Donation",
+        interval: "month",
+        amount: input.amount
+      });
+      console.log("PLAN", varPlan);
+      // Create Customer
+      const customer = await stripe.customers.create({
+        email: input.email,
         source: input.token
       });
-      console.log(status);
+      console.log("CUSTOMER", customer);
+      // Store Customer Email and ID in DB
+      const subscription = await stripe.subscriptions.create({
+        customer: customer.id,
+        items: [{ plan: varPlan.id }]
+      });
+      console.log("SUBSCRIPTION", subscription);
+      // let { status } = await stripe.charges.create({
+      //   receipt_email: input.email,
+      //   amount: input.amount,
+      //   currency: "usd",
+      //   description: "Jijenge Donation",
+      //   source: input.token
+      // });
+      console.log(subscription.status);
       success("Successfully processed data from payment controller");
-      res.json({ status });
+      res.json(subscription.status);
     } catch (err) {
       error("Error from payment controller: ", err);
       res.status(500).end();
